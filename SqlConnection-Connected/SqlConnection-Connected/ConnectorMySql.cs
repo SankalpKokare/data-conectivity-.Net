8b07b4319040b1d507ae85af7e83723d3d7856d9
Sankalp Kokare <sankalpkokare21@gmail.com>
2024-01-01 23:50:17 +0530

    Connected to mysql and added display Customer function

---
+++ SqlConnection-Connected/SqlConnection-Connected/ConnectorMySql.cs
@@ -0,0 +1,50 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlConnection_Connected
+{
+    //Added Nuget Package 'MySqlConnector'
+    internal class ConnectorMySql
+    {
+
+        static string cs = "server=localhost;uid=root;password=root;database=sakila";
+
+        public static void displayAllCustomer()
+        {
+
+
+            using (MySqlConnection con = new MySqlConnection(cs))
+            {
+                con.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand())
+                {
+                    cmd.Connection = con;
+                    cmd.CommandText = "select * from customer";
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            Console.WriteLine(reader["first_name"].ToString() + " " + reader["last_name"].ToString());
+                        }
+
+                    }
+                }
+
+            }
+
+        }
+
+        public static void Main()
+        {
+            displayAllCustomer();
+        }
+    }
+    
+}
